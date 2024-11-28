3973059b18a25ff729788cd99e05428f3d51ece4
Candela Mathieu <candemathieu12@gmail.com>
2024-11-28 17:21:52 -0300

    Agrego config de variables de entorno y script start

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,13 @@
 import { createPool } from 'mysql2/promise';
-import dotenv from 'dotenv';
 
-dotenv.config(); 
+
+import { DB_HOST, DB_USER, DB_PASSWORD,DB_PORT, DB_NAME} from "./config.js";
+
+
 export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 3306,
-  database: process.env.DB_NAME,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT || 3306,
+  database: DB_NAME,
 });

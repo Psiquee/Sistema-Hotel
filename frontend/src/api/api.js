471df396bef78fa52c636e8001fcd90900205f32
Candela Mathieu <candemathieu12@gmail.com>
2024-12-06 15:34:10 -0300

    agrego variable de entorno

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,11 @@
-//frontend/src/api/api.js
-
 import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: API_URL,
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
